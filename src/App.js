3d86acb2f57c8fa8242a82d566911b3146c9e807
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-03-05 19:06:48 +0330

    task(feature): toggleReminder added

--- src/App.js
+++ src/App.js
@@ -31,11 +31,18 @@ function App() {
     },
   ]);
   const deleteTask = (id) => setTask(tasks.filter((task) => task.id !== id));
+  const toggleReminder = (id) => {
+    setTask(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
   return (
     <div className="container">
       <Header title="reza" />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To show"
       )}
